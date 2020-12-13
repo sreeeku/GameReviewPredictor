82659028a55666652c2fc58a1a40b21e2379fd6e
Sreekar Uppuluri <sreekar.uppuluri@gmail.com>
2020-12-13 17:39:13 -0600

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 
-class RatingForm(FlaskForm):
+class ReviewPredictorForm(FlaskForm):
     review = StringField('Review')
     predict = SubmitField('PREDICT')
